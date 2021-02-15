231e4572553b496d71bc36c44245a1e2a3782785
sallemao <psasa01@gmail.com>
2021-02-15 13:26:41 +0100

    basic setup and homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import React from 'react';
+import Header from './components/Header';
+import Homepage from './pages/Homepage';
+import './default.scss';
 
 function App() {
   return (
     <div className="App">
-      <h2>React APP</h2>
+       <Header />
+      <div className="main">
+     
+      <Homepage />
+      </div>
     </div>
   );
 }

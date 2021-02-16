3d9bafc9b455d0e39a346a4216968b1234903bfa
sallemao <psasa01@gmail.com>
2021-02-16 08:41:37 +0100

    react router / custom layouts / footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss'
+
+const Footer = props => {
+    return (
+        <footer className="footer">
+            <div className="wrapper">
+                (c) SimpleTut 2020
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import './styles.scss';
+import { Link } from 'react-router-dom'
 
 import Logo from './../../assets/logo.png';
+
 
 const Header = props => {
     return (
         <header className="header">
             <div className="wrap">
-                <div className="logo">
-                    <img src={Logo} alt="SimpleTut LOGO" />
+                <Link to="/">
+                    <div className="logo">
+                        <img src={Logo} alt="SimpleTut LOGO" />
+                    </div>
+                </Link>
+                <div className="callToActions">
+                    <ul>
+                        <li>
+                            <Link to="/registration">
+                                Register
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
+
             </div>
         </header>
     )
